b9ab9543817f0286fb60948e52b743ebc73f7589
Sascha Holmes <146263983+gigipink1@users.noreply.github.com>
2023-10-25 21:38:15 +0100

    Refactor gameBoard function into a IIFE

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-function game() {
+/*function game() {
     const gameBoard = (() => {
-        let board = ['X', 'X', 'X','X','X','X','X','X','X'];
+        let board = [{marker: null}, {marker: null}, {marker: null}, {marker: null}, {marker: null}, {marker: null}, {marker: null}, {marker: null}, {marker: null}];
         return board;
     })
     return gameBoard();
-}
+}*/
 
-function buildGame() {
-    const array = game();
-    const body = document.querySelector('.main-container');
-    const div = document.createElement('div');
-    for(square of array){
-       div.classList.add('square');
-       div.textContent = square;
-       body.appendChild(div.cloneNode(1));
+const gameBoard = (function buildGame() {
+    const array = gameSquares();
+    for([index, square] of array.entries()){
+       makeSquare(index, square);
+       
     }
 
-}
+    function makeSquare (index, square){
+        const body = document.querySelector('.main-container');
+        const div = document.createElement('div');
+        div.classList.add('square');
+        div.dataset.indexNumber = index;
+       body.appendChild(div.cloneNode(1));
+       console.log(index, square)
+    }
+    function gameSquares() {
+        const gameBoard = (() => {
+            let board = [{marker: null}, {marker: null}, {marker: null}, {marker: null}, {marker: null}, {marker: null}, {marker: null}, {marker: null}, {marker: null}];
+            return board;
+        })
+        return gameBoard(); }
 
-buildGame();
+    return {buildGame, gameSquares}
+
+    
+
+})(); 
+
+
